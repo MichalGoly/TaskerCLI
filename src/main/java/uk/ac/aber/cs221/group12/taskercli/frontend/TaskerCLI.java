0b417c8343843cf0b8e52dea92ce8f02b9ba8029
Joshua Mir <joshuam10o@gmail.com>
2015-11-22 15:56:02 +0000

    Added new ui class

---
+++ src/main/java/uk/ac/aber/cs221/group12/taskercli/frontend/TaskerCLI.java
@@ -0,0 +1,14 @@
+package uk.ac.aber.cs221.group12.taskercli.frontend;
+
+public class TaskerCLI {
+
+	public TaskerCLI() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
