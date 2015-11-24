25994d20fdb925520974c4b70f637a8562e2a0c9
MichalGoly <m.goly@goly2.com>
2015-11-24 10:41:58 +0000

    very basic table populated with TeamMember data

--- src/main/java/uk/ac/aber/cs221/group12/taskercli/frontend/MainFrame.java
+++ src/main/java/uk/ac/aber/cs221/group12/taskercli/frontend/MainFrame.java
@@ -1,14 +1,26 @@
 package uk.ac.aber.cs221.group12.taskercli.frontend;
 
 import java.awt.GridBagLayout;
+import java.util.Vector;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import uk.ac.aber.cs221.group12.taskercli.business.Task;
+import uk.ac.aber.cs221.group12.taskercli.business.TeamMember;
+import uk.ac.aber.cs221.group12.taskercli.data.TeamMemberDB;
+import uk.ac.aber.cs221.group12.taskercli.util.GBC;
 
 /**
  *
  * @author Michal Goly
  */
 public class MainFrame extends JFrame {
-      
+   
+   private SidebarPanel sidebarPanel;
+   private JTable table;
+   
    public MainFrame() {
       initComponents();
       initFrame();
@@ -16,12 +28,47 @@ public class MainFrame extends JFrame {
 
    private void initComponents() {
       setLayout(new GridBagLayout());
+      sidebarPanel = new SidebarPanel();
+      add(sidebarPanel, new GBC(0, 0, 4, 1).setWeight(0, 0)
+              .setFill(GBC.HORIZONTAL));
       
+      // Syncer code should take care of logging in
+      TeamMember teamMember = null;
+      try {
+         teamMember = TeamMemberDB.selectTeamMemberByEmail("m.goly@goly2.com");
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
       
+      JScrollPane scrollPane = new JScrollPane(new JTable(createModel(teamMember)));
+      add(scrollPane, new GBC(0, 4, 12, 8).setFill(GBC.BOTH).setWeight(100, 100));
    }
 
    private void initFrame() {
-      throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      pack();
+      setLocationRelativeTo(null);
+      setTitle("TaskerCLI");
+      setVisible(true);
+   }
+
+   private TableModel createModel(TeamMember teamMember) {
+      Vector<String> columnNames = new Vector<>();
+      columnNames.add("Task");
+      columnNames.add("Start Date");
+      columnNames.add("End Date");
+      columnNames.add("Status");
+      
+      Vector<Vector<Object>> data = new Vector<>();
+      for (Task t : teamMember.getTaskList()) {
+          Vector<Object> row = new Vector<>();
+          row.add(t.getTitle());
+          row.add(t.getStartDate());
+          row.add(t.getEndDate());
+          row.add(t.getStatus().toString());
+          data.add(row);
+      }
+      
+      return new DefaultTableModel(data, columnNames);
    }
    
    
--- src/main/java/uk/ac/aber/cs221/group12/taskercli/frontend/SidebarPanel.java
+++ src/main/java/uk/ac/aber/cs221/group12/taskercli/frontend/SidebarPanel.java
@@ -1,5 +1,6 @@
 package uk.ac.aber.cs221.group12.taskercli.frontend;
 
+import java.awt.Dimension;
 import javax.swing.JPanel;
 
 /**
@@ -11,6 +12,8 @@ public class SidebarPanel extends JPanel {
    public static final int DEFAULT_WIDTH = 200;
    public static final int DEFAULT_HEIGHT = 600;
    
-   
+   public SidebarPanel() {
+      setSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
+   }
    
 }
