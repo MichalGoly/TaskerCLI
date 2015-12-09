9ea53eaa148316e156758fa27653293f14017713
MichalGoly <m.goly@goly2.com>
2015-12-09 16:52:17 +0000

    initial table model in main frame

--- src/main/java/uk/ac/aber/cs221/group12/taskercli/frontend/MainFrame.java
+++ src/main/java/uk/ac/aber/cs221/group12/taskercli/frontend/MainFrame.java
@@ -1,29 +1,36 @@
 package uk.ac.aber.cs221.group12.taskercli.frontend;
 
-import java.awt.*;
+import java.awt.GridBagLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.util.List;
 import java.util.Vector;
-import javax.swing.*;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import uk.ac.aber.cs221.group12.taskercli.business.Task;
 import uk.ac.aber.cs221.group12.taskercli.business.TeamMember;
 import uk.ac.aber.cs221.group12.taskercli.util.GBC;
 
+
+
 /**
  *
  * @author Michal Goly
  */
 public class MainFrame extends JFrame {
-   
+
    private SidebarPanel sidebarPanel;
    private JTable table;
    private TaskFrame taskFrame;
-   
+
    private TeamMember teamMember;
-   
+
    public MainFrame(TeamMember teamMember) {
       this.teamMember = teamMember;
       initComponents();
@@ -38,7 +45,7 @@ public class MainFrame extends JFrame {
       add(sidebarPanel, new GBC(0, 0, 4, 1).setWeight(0, 0)
               .setFill(GBC.BOTH));
 
-      table = new JTable(createModel(teamMember));
+      table = new JTable(new TaskTableModel(teamMember));
       table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
       table.setAutoCreateRowSorter(true);
       table.addMouseListener(new MouseAdapter() {
@@ -50,7 +57,7 @@ public class MainFrame extends JFrame {
       });
       JScrollPane scrollPane = new JScrollPane(table);
       add(scrollPane, new GBC(4, 0, 8, 8).setFill(GBC.BOTH).setWeight(100, 100));
-      
+
    }
 
    private void initFrame() {
@@ -59,28 +66,67 @@ public class MainFrame extends JFrame {
       setTitle("Tasker - Welcome, " + teamMember.getFirstName());
       setVisible(true);
    }
+   
+   private class TaskTableModel extends AbstractTableModel {
 
-   private TableModel createModel(TeamMember teamMember) {
-      Vector<String> columnNames = new Vector<>();
-      columnNames.add("Task");
-      columnNames.add("Start Date");
-      columnNames.add("End Date");
-      columnNames.add("Sub-tasks");
-      columnNames.add("Status");
-      
-      Vector<Vector<Object>> data = new Vector<>();
-      for (Task t : teamMember.getTaskList()) {
-          Vector<Object> row = new Vector<>();
-          row.add(t.getTitle());
-          row.add(t.getStartDate());
-          row.add(t.getEndDate());
-          row.add(t.getTaskElementList().size());
-          row.add(t.getStatus().toString());
-          data.add(row);
+      private List<Task> tasks;
+
+      public TaskTableModel(TeamMember teamMember) {
+
+         this.tasks = teamMember.getTaskList();
+
       }
 
-      return new DefaultTableModel(data, columnNames);
+      @Override
+      public int getRowCount() {
+         return tasks.size();
+      }
+
+      @Override
+      public int getColumnCount() {
+         return 5;
+      }
+
+      @Override
+      public Object getValueAt(int rowIndex, int columnIndex) {
+
+         Object value = "??";
+         Task task = tasks.get(rowIndex);
+         switch (columnIndex) {
+            case 0:
+               value = task.getTitle();
+               break;
+            case 1:
+               value = task.getStartDate().toString();
+               break;
+            case 2:
+               value = task.getEndDate().toString();
+               break;
+            case 3:
+               value = task.getTaskElementList().size();
+               break;
+            case 4:
+               value = task.getStatus().toString();
+               break;
+         }
+
+         return value;
+      }
+
+      @Override
+      public Class<?> getColumnClass(int columnIndex) {
+         return String.class;
+      }
+
+      /**
+       * This will return the user at the specified row...
+       *
+       * @param row
+       * @return
+       */
+      public Task getTaskAt(int row) {
+         return tasks.get(row);
+      }
+
    }
-   
-   
 }
