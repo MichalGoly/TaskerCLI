b60ecd75e4a3dbb0fe19021241b670e8f0d24126
Joshua Mir <joshuam10o@gmail.com>
2016-01-23 13:38:31 +0000

    ProgressBar started

---
+++ src/main/java/uk/ac/aber/cs221/group12/taskercli/frontend/ProgressBar.java
@@ -0,0 +1,31 @@
+package uk.ac.aber.cs221.group12.taskercli.frontend;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by jam on 1/23/16.
+ */
+
+
+public class ProgressBar extends JPanel {
+    private JProgressBar progressBar;
+    private JLabel textArea;
+    private String status;
+
+    public ProgressBar(){
+        super(new BorderLayout());
+        progressBar = new JProgressBar();
+        progressBar.setIndeterminate(true);
+
+        status = new String();
+
+        textArea = new JLabel(status);
+
+        JPanel panel = new JPanel();
+        panel.add(progressBar, BorderLayout.CENTER);
+        panel.add(textArea, BorderLayout.SOUTH);
+    }
+
+
+}
