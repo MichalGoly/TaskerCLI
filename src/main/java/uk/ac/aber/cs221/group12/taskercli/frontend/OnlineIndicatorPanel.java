8a20af67c2b0e99d3a6fa8af90a52a2e0fa6e305
tam15 <tam15@aber.ac.uk>
2016-01-25 11:41:14 +0000

    Online indicator font size changed

--- src/main/java/uk/ac/aber/cs221/group12/taskercli/frontend/OnlineIndicatorPanel.java
+++ src/main/java/uk/ac/aber/cs221/group12/taskercli/frontend/OnlineIndicatorPanel.java
@@ -1,14 +1,8 @@
 package uk.ac.aber.cs221.group12.taskercli.frontend;
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Image;
-
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
-import uk.ac.aber.cs221.group12.taskercli.business.TeamMember;
 
 public class OnlineIndicatorPanel extends JPanel {
 
@@ -16,7 +10,6 @@ public class OnlineIndicatorPanel extends JPanel {
 	 * Create the panel.
 	 */
 	
-	private TeamMember teamMember;
 	private JLabel label;
 	
 	public OnlineIndicatorPanel() {
@@ -32,12 +25,14 @@ public class OnlineIndicatorPanel extends JPanel {
 	
 	public void setOnline(){
 		label.setText("Online");
+		label.setFont(label.getFont().deriveFont(15f));
 		label.setForeground(Color.green);
 	    
 	}
 	
 	public void setOffline(){
 		label.setText("Offline");
+		label.setFont(label.getFont().deriveFont(15f));
 		label.setForeground(Color.red);
 	}
 
