a9cc7369ea2896d529b8263b179237f22b071487
Adam <adn2@aber.ac.uk>
2016-01-26 11:14:26 +0000

    Added test classes for every class

---
+++ src/test/java/uk/ac/aber/cs211/group12/taskercli/frontend/TaskerCLITest.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.aber.cs211.group12.taskercli.frontend;
+
+/**
+ *
+ * @author adn2
+ */
+public class TaskerCLITest {
+    
+}
