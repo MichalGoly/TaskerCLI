aae82409edd73ac906d60c0a8e413162005af97c
tam15 <tam15@aber.ac.uk>
2016-01-27 12:26:41 +0000

    SidebarPanelTest Class started

--- src/test/java/uk/ac/aber/cs211/group12/taskercli/frontend/SidebarPanelTest.java
+++ src/test/java/uk/ac/aber/cs211/group12/taskercli/frontend/SidebarPanelTest.java
@@ -7,10 +7,59 @@
  */
 package uk.ac.aber.cs211.group12.taskercli.frontend;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JFrame;
+import javax.swing.JTable;
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import uk.ac.aber.cs221.group12.taskercli.business.Task;
+import uk.ac.aber.cs221.group12.taskercli.business.TaskElement;
+import uk.ac.aber.cs221.group12.taskercli.business.TaskStatus;
+import uk.ac.aber.cs221.group12.taskercli.business.TeamMember;
+import uk.ac.aber.cs221.group12.taskercli.frontend.MainFrame;
+import uk.ac.aber.cs221.group12.taskercli.frontend.SidebarPanel;
+
 /**
  *
- * @author adn2
+ * @author Tom Mills
  */
+
 public class SidebarPanelTest {
+    private TeamMember teamMember;
+    private JFrame testFrame;
+    private JTable table = new JTable();
+    
+    @Before
+    public void createTeamMember() {
+        List<TaskElement> taskElements = new ArrayList<>();
+        taskElements.add(new TaskElement(20L, "Go to train station", ""));
+        taskElements.add(new TaskElement(21L, "Get inside the train", ""));
+        List<Task> tasks = new ArrayList<>();
+        Task task = new Task(7L, "Go back for Christmas",
+                new Date(2011, 2, 7),
+                new Date(2010, 2, 1),
+                TaskStatus.ALLOCATED, taskElements);
+
+        tasks.add(task);
+        teamMember =
+                new TeamMember("Mark", "Smith", "mark@smith.com", "fish", tasks);
+    }
+    
+    @Test
+    public void assertIsSidebarVisible(){
+        testFrame = new JFrame();
+        testFrame.setVisible(true);
+        SidebarPanel sidebar = new SidebarPanel(table,teamMember);
+        testFrame.add(sidebar);
+        sidebar.setVisible(true);
+        Assert.assertEquals("Sidebar not visible", true, sidebar.isShowing());
+    }
+    
+
+    
+    
     
 }
