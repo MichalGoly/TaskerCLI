e03a1148b8219435a04b91ccf5a5cfd106333375
Joshua Mir <joshuam10o@gmail.com>
2016-01-27 11:03:03 +0000

    TaskElementTest written

--- src/test/java/uk/ac/aber/cs211/group12/taskercli/business/TaskElementTest.java
+++ src/test/java/uk/ac/aber/cs211/group12/taskercli/business/TaskElementTest.java
@@ -7,10 +7,29 @@
  */
 package uk.ac.aber.cs211.group12.taskercli.business;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import uk.ac.aber.cs221.group12.taskercli.business.TaskElement;
+
 /**
  *
  * @author adn2
  */
 public class TaskElementTest {
+
+    TaskElement taskElement;
+
+    @Before
+    public void createTaskElement() {
+        taskElement = new TaskElement(20L, "Go to train station", "");
+    }
+
+    @Test
+    public void verifyTaskElement() {
+        Assert.assertEquals("TaskElement must have correct description", taskElement.getDescription(), "Go to train station");
+        Assert.assertEquals("TaskElement must have correct comment", taskElement.getComments(), "");
+        Assert.assertEquals("TaskElement id must be correct", taskElement.getTaskElementId(), new Long(20));
+    }
     
 }
