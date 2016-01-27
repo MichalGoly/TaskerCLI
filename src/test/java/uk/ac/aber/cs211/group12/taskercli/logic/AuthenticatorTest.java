57a875095b4fa01ad51a3a704c776359f0f17067
Joshua Mir <joshuam10o@gmail.com>
2016-01-27 09:14:41 +0000

    added header

--- src/test/java/uk/ac/aber/cs211/group12/taskercli/logic/AuthenticatorTest.java
+++ src/test/java/uk/ac/aber/cs211/group12/taskercli/logic/AuthenticatorTest.java
@@ -1,3 +1,11 @@
+/*
+* @(#) AuthenticatorTest.java 1.0 26/01/16
+*
+* Copyright (c) 2016 Aberystwyth University.
+* All rights reserved.
+*
+*/
+
 package uk.ac.aber.cs211.group12.taskercli.logic;
 
 /**
