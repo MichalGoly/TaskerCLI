44843dc91a53d69a14251c7bc2681ef54a6873fa
tam15 <tam15@aber.ac.uk>
2016-01-27 16:12:38 +0000

    TaskDBTest searchTaskByID test implemented

--- src/test/java/uk/ac/aber/cs221/group12/taskercli/data/TaskDBTest.java
+++ src/test/java/uk/ac/aber/cs221/group12/taskercli/data/TaskDBTest.java
@@ -7,9 +7,66 @@
  */
 package uk.ac.aber.cs221.group12.taskercli.data;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import uk.ac.aber.cs221.group12.taskercli.business.Task;
+import uk.ac.aber.cs221.group12.taskercli.business.TaskElement;
+import uk.ac.aber.cs221.group12.taskercli.business.TaskStatus;
+
 /**
- * @author adn2
+ * @author Tom Mills
  */
 public class TaskDBTest {
+    
+    private Connection conn;
+    private Properties props;
+    private Task task;
+    private Task selectedTask;
+    private TaskDB taskDB;
+    private String email = "m.goly@goly2.com";
+    private List<Task> tasks;
+    private List<Task> actualTasks;
+    
+    @Before
+    public void createTask(){
+       Long id = new Long(1);
+        String title = "Do shopping";
+        Date startDate = new Date(115,8,29);
+        Date endDate = new Date(115,9,30);
+        TaskStatus status = TaskStatus.ALLOCATED;
+        List<TaskElement> elements = new ArrayList<TaskElement>();
+
+        TaskElement element1 = new TaskElement(new Long(1),"get out of the house","");
+        elements.add(element1);
+        TaskElement element2 = new TaskElement(new Long(2),"drive to the store","");
+        elements.add(element2);
+        TaskElement element3 = new TaskElement(new Long(3),"put items to your basket","");
+        elements.add(element3);
+        TaskElement element4 = new TaskElement(new Long(4),"pay for the goods","");
+        elements.add(element4);
+        TaskElement element5 = new TaskElement(new Long(5),"drive back home","");
+        elements.add(element5);
+        
+        task = new Task(id,title,startDate,endDate,status,elements);
+    }
+    
+    @Test
+    public void assertCanSearchTaskById(){
+        taskDB=new TaskDB();
+        try{
+        selectedTask = taskDB.selectTaskById(1,1);
+        }catch(Exception e){
+            System.err.println("Error selecting task");
+        }
+        Assert.assertEquals(task, selectedTask);
+    }
+    
 
 }
+
