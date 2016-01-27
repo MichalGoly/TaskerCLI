f2a8517777008258da6d6f024b5a744e15fbf9f1
Joshua Mir <joshuam10o@gmail.com>
2016-01-27 10:50:59 +0000

    TaskStatusTest written

--- src/test/java/uk/ac/aber/cs211/group12/taskercli/business/TaskStatusTest.java
+++ src/test/java/uk/ac/aber/cs211/group12/taskercli/business/TaskStatusTest.java
@@ -7,10 +7,23 @@
  */
 package uk.ac.aber.cs211.group12.taskercli.business;
 
+import org.junit.Assert;
+import org.junit.Test;
+import uk.ac.aber.cs221.group12.taskercli.business.TaskStatus;
+
 /**
  *
  * @author adn2
  */
 public class TaskStatusTest {
-    
+
+    @Test
+    public void assertEnumCorrect() {
+        Assert.assertEquals("Task allocated correct value", TaskStatus.fromInt(1), TaskStatus.ALLOCATED);
+
+        Assert.assertEquals("Task abandoned correct value", TaskStatus.fromInt(2), TaskStatus.ABANDONED);
+
+        Assert.assertEquals("Task completed correct value", TaskStatus.fromInt(3), TaskStatus.COMPLETED);
+
+    }
 }
