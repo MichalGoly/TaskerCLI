2f321f04ac8624dfa73f228dfc845768f87061ec
Adam <adn2@aber.ac.uk>
2016-01-28 12:46:09 +0000

    Authenticator now has javadoc

--- src/main/java/uk/ac/aber/cs221/group12/taskercli/logic/Authenticator.java
+++ src/main/java/uk/ac/aber/cs221/group12/taskercli/logic/Authenticator.java
@@ -12,16 +12,25 @@ import org.mindrot.jbcrypt.BCrypt;
 import uk.ac.aber.cs221.group12.taskercli.business.TeamMember;
 
 /**
- *
+ * Authenticator provides a static method to provide functionality to the 
+ * {@link uk.ac.aber.cs221.group12.taskercli.logic.Syncer#logIn() } method to
+ * check passwords. Using {@link BCrypt} the passwords are hashed to keep
+ * them secure, and are encrypted in the databases. 
+ * 
  * @author Michal Goly
+ * @see TeamMember
+ * @see BCrypt
+ * @see uk.ac.aber.cs221.group12.taskercli.logic.Syncer#logIn()
  */
 public class Authenticator {
 
    /**
+    * Used to encrypt the entered password on the login box, and compare it
+    * to the matching encrypted password inside the database.
     *
-    *
-    * @param teamMember
-    * @param password
+    * @param teamMember The TeamMember whose password needs checking.
+    * @param password The password entered into the login box, to be encrypted
+    * and compared
     * @return true if the password provided matches the password in the TeamMember
     */
    public static boolean authenticate(TeamMember teamMember, char[] password) {
